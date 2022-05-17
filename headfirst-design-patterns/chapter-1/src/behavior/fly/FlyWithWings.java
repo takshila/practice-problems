fe2209a7f7c01517e4bf3c21a7f356016af0d6ec
Saurabh Seth <6327636+takshila@users.noreply.github.com>
2022-05-17 16:16:04 -0700

    Add different flybehaviors

---
+++ headfirst-design-patterns/chapter-1/src/behavior/fly/FlyWithWings.java
@@ -0,0 +1,8 @@
+package behavior.fly;
+
+public class FlyWithWings implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with my wings!!");
+    }
+}
